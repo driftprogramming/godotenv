c0fd2086b796a0285fcc5738c20b412e64e708ee
Hejun Deng <denghejun@live.cn>
2021-07-05 20:26:27 +0800

    init godotenv

---
+++ godotenv_test.go
@@ -0,0 +1,56 @@
+package godotenv
+
+import (
+	"embed"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed envs/*
+var envFS embed.FS
+
+func Test_In_UnParallel(t *testing.T) {
+	t.Parallel()
+
+	err := testLoadShouldGetEmbeddedLocalEnvFileSuccessfully()
+	assert.Nil(t, err)
+	assert.Equal(t, "local.1.0", os.Getenv("version"))
+
+	err = testLoadShouldGetEmbeddedLiveeEnvFileSuccessfully()
+	assert.Nil(t, err)
+	assert.Equal(t, "live.1.0", os.Getenv("version"))
+
+	err = testLoadShouldGetEmbeddedCommonEnvFileSuccessfully()
+	assert.Nil(t, err)
+	assert.Equal(t, "abcd", os.Getenv("shared_key"))
+
+	testLoadShouldGetEmbeddedCommonAndLiveEnvFileSuccessfully()
+	assert.Equal(t, "abcd", os.Getenv("shared_key"))
+	assert.Equal(t, "live.1.0", os.Getenv("version"))
+
+	testLoadTestEnvWithoutOverwriteShouldNotBeOverwrite()
+	assert.Equal(t, "live.1.0", os.Getenv("version"))
+}
+
+func testLoadShouldGetEmbeddedLocalEnvFileSuccessfully() error {
+	return Load(envFS, "envs/.env.local")
+}
+
+func testLoadShouldGetEmbeddedLiveeEnvFileSuccessfully() error {
+	return Load(envFS, "envs/.env.live")
+}
+
+func testLoadShouldGetEmbeddedCommonEnvFileSuccessfully() error {
+	return Load(envFS, "envs/.env")
+}
+
+func testLoadShouldGetEmbeddedCommonAndLiveEnvFileSuccessfully() {
+	_ = Load(envFS, "envs/.env")
+	_ = Load(envFS, "envs/.env.live")
+}
+
+func testLoadTestEnvWithoutOverwriteShouldNotBeOverwrite() {
+	_ = LoadWithoutOverwrite(envFS, "envs/.env.test")
+}
